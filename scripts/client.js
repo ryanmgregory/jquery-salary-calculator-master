8aa290af4b28c1364243bac2583609ab4d4f5238
Ryan Gregory <RyanGregoryCodes@gmail.com>
2020-03-14 16:23:34 -0500

    created ready & init & submit capture & employee array

--- scripts/client.js
+++ scripts/client.js
@@ -1,1 +1,15 @@
 console.log("client js");
+
+const employeeArr = [];
+
+$(document).ready(init);
+
+function init() {
+    $(".js-employeeForm-add").on('submit', submitEmployee);
+}
+
+function submitEmployee(event) {
+    event.preventDefault();
+    console.log("in submitEmployee");
+}
+
